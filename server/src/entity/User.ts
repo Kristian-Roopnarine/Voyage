f141ba8345f102c1b2be33fcb94503c721b4ec03
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-16 16:58:21 -0400

    (add) User and Country typegraphql entity and added to apollo server

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,10 +1,19 @@
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, ID } from "type-graphql";
 
 @ObjectType()
 export class User {
-	@Field((type) => ID)
-	id: String;
+  @Field((type) => ID)
+  id: String;
 
-	@Field((type) => String)
-	name: String;
+  @Field((type) => String)
+  firstName: String;
+
+  @Field((type) => String)
+  lastName: String;
+
+  @Field((type) => String)
+  googleId: String;
+
+  @Field((type) => String)
+  email: String;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,25 @@
-import 'reflect-metadata';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
-import { UserResolver } from './resolvers/UserResolver';
+import { UserResolver } from "./resolvers/UserResolver";
 
 const main = async () => {
-	const PORT = process.env.PORT || 4000;
-	const app = express();
+  const PORT = process.env.PORT || 4000;
+  const app = express();
 
-	const apolloServer = new ApolloServer({
-		schema: await buildSchema({
-			resolvers: [UserResolver],
-			validate: false,
-		}),
-	});
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+  });
 
-	apolloServer.applyMiddleware({ app });
-	app.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}`);
-	});
+  apolloServer.applyMiddleware({ app });
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 };
 
 main();
