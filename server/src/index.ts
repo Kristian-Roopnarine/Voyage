8da149f026de6c15a4a820a9fd68086d91515c56
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-24 08:33:04 -0400

    (add) city resolver to graphql schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,29 @@
-import "reflect-metadata";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { PrismaClient } from "@prisma/client";
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PrismaClient } from '@prisma/client';
 
-import { UserResolver } from "./resolvers/UserResolver";
+import { UserResolver } from './resolvers/UserResolver';
+import { CityResolver } from './resolvers/CityResolver';
 
 export const prisma = new PrismaClient();
 
 const main = async () => {
-  const PORT = process.env.PORT || 4000;
-  const app = express();
+	const PORT = process.env.PORT || 4000;
+	const app = express();
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [UserResolver],
-      validate: false,
-    }),
-  });
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [UserResolver, CityResolver],
+			validate: false,
+		}),
+	});
 
-  apolloServer.applyMiddleware({ app });
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+	apolloServer.applyMiddleware({ app });
+	app.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`);
+	});
 };
 
 main();
